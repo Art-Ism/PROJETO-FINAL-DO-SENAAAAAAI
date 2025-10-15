cc2fdf68fe25556bb460a72e8a26cc643eca7012
Art_ism <tutuoyama@gmail.com>
2025-10-14 21:48:28 -0300

    inicio da aplicação

---
+++ core/aluno/aluno_repository.py
@@ -0,0 +1,10 @@
+
+class AlunoRepository:
+    def __init__(self, db_name=""):
+        self.db_name = db_name
+
+    def listar(self):
+        pass
+
+    def adicinOar(self, aluno):
+        pass
